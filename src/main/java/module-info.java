65ab10476f4791530ff7fddb954e228d947f7a91
LELE <LELE@localhost>
2024-11-22 15:43:13 +0100

    Aggiunto println di prova2.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module com.mycompany.rubrica_gruppo12 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.mycompany.rubrica_gruppo12 to javafx.fxml;
+    exports com.mycompany.rubrica_gruppo12;
+}
+*/
