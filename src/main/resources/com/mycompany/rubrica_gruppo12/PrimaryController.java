a7a725bbb3290ed82477932888f8c0eb716dd299
LELE <LELE@localhost>
2024-12-04 09:30:21 +0100

    .

---
+++ src/main/resources/com/mycompany/rubrica_gruppo12/PrimaryController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.rubrica_gruppo12;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author LELE
+ */
+public class PrimaryController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
