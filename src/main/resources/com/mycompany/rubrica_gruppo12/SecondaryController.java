f2ccd28166d0877cd5171aa8a9df7b6fd263fbaf
gabrieledilieto1 <g.dilieto5@studenti.unisa.it>
2024-12-05 16:10:46 +0100

    Interfaccia: SecondaryController

--- src/main/resources/com/mycompany/rubrica_gruppo12/SecondaryController.java
+++ src/main/resources/com/mycompany/rubrica_gruppo12/SecondaryController.java
@@ -7,7 +7,11 @@ package com.mycompany.rubrica_gruppo12;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -15,6 +19,41 @@ import javafx.fxml.Initializable;
  * @author LELE
  */
 public class SecondaryController implements Initializable {
+
+    @FXML
+    private TextField txtNome;
+    @FXML
+    private TextField txtCognome;
+    @FXML
+    private TextField txtNumTelefono;
+    @FXML
+    private TextField txtEmail;
+    @FXML
+    private Button btnAggiungi;
+    @FXML
+    private TextField txtCerca;
+    @FXML
+    private Button btnCerca;
+    @FXML
+    private Button btnTornaIndietro;
+    @FXML
+    private Label lblNomeContatto;
+    @FXML
+    private Label lblCognomeContatto;
+    @FXML
+    private Label lblNumtelefono1;
+    @FXML
+    private Label lblNumtelefono2;
+    @FXML
+    private Label lblNumtelefono3;
+    @FXML
+    private Label lblEmail1;
+    @FXML
+    private Label lblEmail2;
+    @FXML
+    private Label lblEmail3;
+    @FXML
+    private Button btnModifica;
 
     /**
      * Initializes the controller class.
